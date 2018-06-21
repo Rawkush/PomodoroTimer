b689ffdb898eebb013e9f2c4c48c78c8ef3342c0
rawkush <ankushrawat950@gmail.com>
2018-06-21 14:41:14 +0530

    setting layout

--- app/src/main/java/com/example/ankush/pomodorotimer/MainActivity.java
+++ app/src/main/java/com/example/ankush/pomodorotimer/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.CountDownTimer;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,23 +13,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        SeekBar timerSeekbar=(SeekBar) findViewById(R.id.seekBar);
+        timerSeekbar.setMax(600);
+        timerSeekbar.setProgress(30);
+
+
+
+
        // using alternative method
 
             new CountDownTimer(10000,1000){  // first param is timer till which to count
                                                     // second param is in what intervals should it count
-
                 @Override
                 public void onTick(long millisUntilFinished) {
 
                     // after every second
 
                 }
-
                 @Override
                 public void onFinish() {
                     // when conter is finished(after 10 sec)
                 }
-            };
+            }.start();
        /*
         final Handler handler= new Handler(); // it allows delaying
         Runnable run=new Runnable() {  //chunk of code that handler handles is called runnable
