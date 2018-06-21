5b085964dc9640a0c750bdf095261a93cde4ed6f
rawkush <ankushrawat950@gmail.com>
2018-06-21 11:19:00 +0530

    initial commit

---
+++ app/src/main/java/com/example/ankush/pomodorotimer/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.ankush.pomodorotimer;
+
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Handler handler= new Handler(); // it allows delaying
+
+    }
+
+
+}
